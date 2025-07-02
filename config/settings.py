0f55a7cf5d3c666558b28eff4f2e54cae73e463a
mrdongha <hakhanhhmt@gmail.com>
2025-07-02 09:44:24 +0700

    Final version for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -1,34 +1,15 @@
-# config/settings.py
 import os
 from pathlib import Path
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Bạn có thể đặt một khóa bí mật bất kỳ ở đây
-SECRET_KEY = 'a-secure-key-for-pythonanywhere-deployment'
-
-# Tắt DEBUG khi chạy thật
+SECRET_KEY = 'a-secure-secret-key-for-your-final-deployment'
 DEBUG = False
-
-# ✅ DÒNG NÀY SẼ SỬA LỖI. Thay 'hadong' bằng username của bạn
-ALLOWED_HOSTS = ['hadong.pythonanywhere.com']
-
-# Application definition
-INSTALLED_APPS = [
-    'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','whitenoise.runserver_nostatic','django.contrib.staticfiles','django.contrib.humanize','sales',
-]
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware','whitenoise.middleware.WhiteNoiseMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+ALLOWED_HOSTS = ['hadong.pythonanywhere.com'] # Thay 'hadong' bằng username của bạn
+INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','whitenoise.runserver_nostatic','django.contrib.staticfiles','django.contrib.humanize','sales',]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','whitenoise.middleware.WhiteNoiseMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware',]
 ROOT_URLCONF = 'config.urls'
 TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates','DIRS': [],'APP_DIRS': True,'OPTIONS': {'context_processors': ['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages',],},},]
 WSGI_APPLICATION = 'config.wsgi.application'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}}
 AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},{'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},{'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},]
 LANGUAGE_CODE = 'en-us'; TIME_ZONE = 'UTC'; USE_I18N = True; USE_TZ = True
 STATIC_URL = 'static/'; STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles');
